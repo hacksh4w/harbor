ea3afd58cb191e022e5991e8488369a6a9719a5c
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-06 02:16:57 +0530

    landing page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useContext, createContext, useEffect } from "react";
-import './App.css'
+import React, {useState, useContext, createContext, useEffect} from "react";
+import "./App.css";
 
 export default function App() {
-{/* initialize();  for firebase */}
-  
+  {
+    /* initialize();  for firebase */
+  }
+
   return (
+    <>
+      <h1>SheHacks Project</h1>
+      <button className="red">Report Abuse</button>
       <div>
-       {/*} <Routes>
+        {/*} <Routes>
           <Route path="/" element={<Landing />}></Route>
           <Route path="/signin" element={<SignIn />}></Route>
           
   </Routes> */}
       </div>
+    </>
   );
 }
-
