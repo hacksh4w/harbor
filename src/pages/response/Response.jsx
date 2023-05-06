8f7ceb83a05a53eb784e948dac558b2e93eb4b58
Alaka A J <alaka03aj@gmail.com>
2023-05-07 00:45:49 +0530

    report submit image added

--- src/pages/response/Response.jsx
+++ src/pages/response/Response.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import imag from '../../assets/report-submit-img.png'
 
 export const Response = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <img src={imag} alt="report-submitted-image" className="w-20 h-auto " />
       <h1 className="text-3xl font-bold mb-4 text-black">Your report has been submitted successfully</h1>
       <p className="text-lg text-black">Please stand by for authorities.</p>
     </div>
