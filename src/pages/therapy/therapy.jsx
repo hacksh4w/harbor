2444a250dfcf1e08b7b4fac447d13c201fbc1ffb
Alaka A J <alaka03aj@gmail.com>
2023-05-06 20:02:29 +0530

    therapy structure

---
+++ src/pages/therapy/therapy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import thcard from '../../components/thcard/thcard'
+const therapy = () => {
+  return (
+    <div>
+      <thcard thname={"John Doe"} thdesignation={"Child Specialist"} thexperience={10} thavail={"Mondays and Thursdays"}/>
+    </div>
+  )
+}
+
+export default therapy
